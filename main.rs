eb0d9dcaebb4039b0054c1849d67face4b2afd4e
Leonardo <leonardo.lv25.25@gmail.com>
2024-01-31 15:36:58 -0300

    algoritimo randomizado para merge de rotas

--- main.rs
+++ main.rs
@@ -1,9 +1,15 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io;
 use std::io::Read;
 use std::io::Write;
+use std::time::Instant;
 
 const MAX_CAPACITY: i32 = 100;
+
+const ALFA_ITERATIONS: i32 = 100;
+const RANDOM_ITERATIONS: i32 = 10;
+const RANDOM_ALFA: bool = false;
 
 // (id, x, y, demand)
 type Node = (i32, i32, i32, i32);
@@ -116,7 +122,7 @@ fn is_node_in_route(node: &Node, route: &Route) -> bool {
     route.0.iter().any(|n| is_node_equal(node, n))
 }
 
-fn merge_routes(route1: &Route, route2: &Route, distance_matrix: &DistanceMatrix) -> Route {
+fn _merge_routes(route1: &Route, route2: &Route, distance_matrix: &DistanceMatrix) -> Route {
     let origin_node: &Node = &route1.0[0];
     let mut new_route: Route = (Vec::new(), 0.0, route1.2 + route2.2);
 
@@ -130,7 +136,6 @@ fn merge_routes(route1: &Route, route2: &Route, distance_matrix: &DistanceMatrix
     }
 
     new_route.0.push(*origin_node);
-
 
     while new_route.0.len() < (all_nodes.len() - 3) {
         let mut min_distance = std::f64::MAX;
@@ -156,7 +161,6 @@ fn merge_routes(route1: &Route, route2: &Route, distance_matrix: &DistanceMatrix
 
         new_route.0.push(*min_node);
         new_route.1 += min_distance;
-        new_route.2 += min_node.3;
     }
 
     // add the distance to the origin node
@@ -169,7 +173,7 @@ fn merge_routes(route1: &Route, route2: &Route, distance_matrix: &DistanceMatrix
     new_route
 }
 
-fn calculate_savings(routes: &Routes, distance_matrix: &DistanceMatrix) -> Savings {
+fn _calculate_savings_lento(routes: &Routes, distance_matrix: &DistanceMatrix) -> Savings {
     let mut savings: Savings = Vec::new();
 
     for i in 0..routes.len() {
@@ -187,7 +191,13 @@ fn calculate_savings(routes: &Routes, distance_matrix: &DistanceMatrix) -> Savin
             let original_distance = routes[i].1 + routes[j].1;
 
             // merge routes
-            let new_route = merge_routes(&routes[i], &routes[j], &distance_matrix);
+            // let new_route = _merge_routes(&routes[i], &routes[j], &distance_matrix);
+            let new_route = _merge_routes_randomized(
+                &routes[i],
+                &routes[j],
+                &distance_matrix,
+                RANDOM_ITERATIONS,
+            );
 
             let new_distance = new_route.1;
 
@@ -201,6 +211,152 @@ fn calculate_savings(routes: &Routes, distance_matrix: &DistanceMatrix) -> Savin
     }
 
     savings
+}
+use indicatif::ProgressBar;
+use indicatif::ProgressStyle;
+use rand::Rng;
+use rayon::prelude::*;
+
+fn quickselect<'a>(arr: &'a mut [(f64, &'a Node)], k: usize) -> Option<(f64, &'a Node)> {
+    let len = arr.len();
+    if len == 0 {
+        return None;
+    }
+    if k >= len {
+        return None;
+    }
+
+    let pivot_index = partition(arr);
+    if k == pivot_index {
+        Some(arr[k])
+    } else if k < pivot_index {
+        quickselect(&mut arr[0..pivot_index], k)
+    } else {
+        quickselect(&mut arr[pivot_index + 1..len], k - pivot_index - 1)
+    }
+}
+
+fn partition(arr: &mut [(f64, &Node)]) -> usize {
+    let len = arr.len();
+    let pivot_index = len / 2;
+    arr.swap(pivot_index, len - 1);
+    let mut i = 0;
+    for j in 0..len - 1 {
+        if arr[j].0 <= arr[len - 1].0 {
+            arr.swap(i, j);
+            i += 1;
+        }
+    }
+    arr.swap(i, len - 1);
+    i
+}
+fn _merge_routes_randomized_stage(
+    route1: &Route,
+    route2: &Route,
+    distance_matrix: &DistanceMatrix,
+    alfa: f32,
+    iterations: i32,
+) -> Route {
+    let mut best_result: Route = (Vec::new(), std::f64::MAX, 0);
+
+    let origin_node: &Node = &route1.0[0];
+    let mut all_nodes: Vec<&Node> = route1.0.iter().chain(route2.0.iter()).collect();
+    all_nodes.retain(|&node| node.0 != origin_node.0);
+    all_nodes.push(origin_node);
+
+    for _i in 0..iterations {
+        let mut new_route: Route = (Vec::new(), 0.0, route1.2 + route2.2);
+        let mut nodes_in_route: HashSet<usize> = HashSet::new();
+
+        new_route.0.push(*origin_node);
+        nodes_in_route.insert(origin_node.0 as usize);
+
+        while new_route.0.len() < all_nodes.len() {
+            let mut node_distances: Vec<(f64, &Node)> = all_nodes
+                .iter()
+                .filter(|&node| !nodes_in_route.contains(&(node.0 as usize)))
+                .map(|&node| {
+                    let last_node_index = new_route.0.last().unwrap().0 as usize;
+                    let distance = distance_matrix[last_node_index - 1][node.0 as usize - 1];
+                    (distance, node)
+                })
+                .collect();
+
+            let selected_node_index = (node_distances.len() as f32 * alfa) as usize;
+            let selected_node = quickselect(&mut node_distances, selected_node_index).unwrap();
+
+            new_route.0.push(*selected_node.1);
+            new_route.1 += selected_node.0;
+            nodes_in_route.insert(selected_node.1 .0 as usize);
+        }
+
+        new_route.1 +=
+            distance_matrix[new_route.0.last().unwrap().0 as usize - 1][origin_node.0 as usize - 1];
+        new_route.0.push(*origin_node);
+
+        if new_route.1 < best_result.1 {
+            best_result = new_route;
+        }
+    }
+
+    best_result
+}
+
+fn _merge_routes_randomized(
+    route1: &Route,
+    route2: &Route,
+    distance_matrix: &DistanceMatrix,
+    iterations: i32,
+) -> Route {
+    let results: Vec<Route> = (1..iterations)
+        // .into_par_iter()
+        .into_iter()
+        .map(|i| {
+            let alfa = if RANDOM_ALFA {
+                i as f32 / iterations as f32
+            } else {
+                rand::thread_rng().gen_range(0.0..1.0)
+            };
+            _merge_routes_randomized_stage(route1, route2, distance_matrix, alfa, ALFA_ITERATIONS)
+        })
+        .collect();
+
+    results
+        .iter()
+        .min_by(|a, b| a.1.partial_cmp(&b.1).unwrap_or(std::cmp::Ordering::Equal))
+        .unwrap()
+        .clone()
+}
+
+fn calculate_savings(routes: &Routes, distance_matrix: &DistanceMatrix) -> Savings {
+    let mut route_pairs: Vec<(usize, usize)> = Vec::new();
+
+    for i in 0..routes.len() {
+        for j in i + 1..routes.len() {
+            if routes[i].2 + routes[j].2 > MAX_CAPACITY {
+                continue;
+            }
+            route_pairs.push((i, j));
+        }
+    }
+
+    let result: Savings = route_pairs
+        .par_iter()
+        .map(|pair| {
+            let original_distance = routes[pair.0].1 + routes[pair.1].1;
+            let new_route = _merge_routes_randomized(
+                &routes[pair.0],
+                &routes[pair.1],
+                &distance_matrix,
+                RANDOM_ITERATIONS,
+            );
+            let new_distance = new_route.1;
+            let saving = original_distance - new_distance;
+            (saving as i32, new_route)
+        })
+        .collect::<Vec<(i32, Route)>>();
+
+    result
 }
 
 /*
@@ -288,9 +444,13 @@ fn remove_routes_dominated(routes: &mut Routes, saving: &Saving) {
 
 fn main() {
     //*  graphs\B-n78-k10.vrp
+    //*  graphs\A-n32-k5.vrp
 
     // read file
     let file_string = read_file();
+    
+    // Start timing
+    let start = Instant::now();
 
     // parse nodes
     let nodes: Nodes = parse_nodes(file_string);
@@ -303,26 +463,25 @@ fn main() {
 
     let mut savings: Savings = calculate_savings(&routes, &distance_matrix);
 
+    let savings_len = savings.len();
+    let pb = ProgressBar::new(savings_len as u64);
+    pb.set_style(
+        ProgressStyle::default_bar()
+            .template(
+                "{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {pos}/{len} ({eta})",
+            )
+            .progress_chars("#>-"),
+    );
+
     while savings.len() > 0 {
-        // println!("n savings: {}", savings.len());
-        // calculate savings
-        
         // get best saving
-        // let mut best_saving: Saving = (0, (Vec::new(), 0.0, 0));
-        
-        // for saving in &savings {
-        //     if saving.0 > best_saving.0 {
-        //         best_saving = saving.clone();
-        //     }
-        // }
+        let mut best_saving: Saving = (0, (Vec::new(), 0.0, 0));
 
-        savings.sort_by(|a, b| b.0.cmp(&a.0));
-
-        
-        let best_saving: Saving = savings.get(0).unwrap().clone();
-
-
-        // println!("best saving: {:?}", best_saving);
+        for saving in &savings {
+            if saving.0 > best_saving.0 {
+                best_saving = saving.clone();
+            }
+        }
 
         if best_saving.0 == 0 {
             break;
@@ -331,12 +490,18 @@ fn main() {
         remove_routes_dominated(&mut routes, &best_saving);
         routes.push(best_saving.1);
 
-        // clean_strange_routes(&mut routes);
-
         savings = calculate_savings(&routes, &distance_matrix);
+
+        pb.inc(1);
+        pb.set_length(savings.len() as u64);
     }
 
+    // End timing
+    let duration = start.elapsed();
+
     generate_graph_file(&nodes, &routes);
+
+    pb.finish_with_message("completed");
 
     println!("Results: ");
     println!("n routes: {}", routes.len());
@@ -344,4 +509,13 @@ fn main() {
         "total distance: {}",
         routes.iter().fold(0.0, |acc, route| acc + route.1)
     );
+    println!(
+        "total demand: {}",
+        routes.iter().fold(0, |acc, route| acc + route.2)
+    );
+    for route in &routes {
+        println!("route: {:?}", route);
+    }
+    // Print the duration in seconds
+    println!("Time elapsed is: {:?}", duration);
 }
